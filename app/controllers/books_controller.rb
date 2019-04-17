597fb9276f591590693f992323ba7f62617b5439
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-04-17 12:23:55 +0900

    [Add]Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,15 +20,23 @@ class BooksController < ApplicationController
 	end
 
 	def show
+		@book = Book.find(params[:id])
 	end
 
 	def edit
+		@book = Book.find(params[:id])
 	end
 
 	def update
+		book = Book.find(params[:id])
+		book.update
+		redirect_to books_path
 	end
 
 	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to book_path
 	end
 
 	private
